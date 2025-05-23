ab5a85728d114a0ca2cb4ccbd2f60125a9675750
Enkhjin Battsetseg <enkhjin_0522a@icloud.com>
2025-05-23 08:29:35 -0400

    added in html instruction to use arrows for manipulating the variable

--- homepage/number.game/number.js
+++ homepage/number.game/number.js
@@ -100,7 +100,7 @@ ctx.fillText("New Numbers", 10, 50);
 ctx.font = "20px Arial";
 ctx.fillText("Guess a number from 1-50 using the up and down arrow keys!", 70, 150);
 ctx.fillText("You have 10 attempts.", 230, 180)
-//defining main game 
+
 
 
 
